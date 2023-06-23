df64871063718d98e309384e908b513f35459d7e
Diego Gonzalez Gualteros <diego.gonzalez.gualteros@ibm.com>
2023-06-23 08:28:21 -0500

    First commit update main.tf

---
+++ main.tf
@@ -0,0 +1,57 @@
+data ibm_resource_group "resource_group" {
+    name = "satellite-rg"
+}
+
+locals {
+  server_count_control_plane = 3
+  server_count_host = 2
+}
+
+resource "ibm_compute_vm_instance" "virtual-server-classic-control-plane" {
+  count                      = local.server_count_control_plane
+  hostname                   = "control-plane-virtual-server-${count.index + 1}"
+  domain                     = "IBM-PoC-Landing-Zone-Enterprise.cloud"
+  os_reference_code          = "REDHAT_8_64"
+  datacenter                 = "dal13"
+  network_speed              = 10
+  hourly_billing             = false
+  private_network_only       = false
+  cores                      = 4
+  memory                     = 16384
+  disks                      = [25, 150]
+  user_metadata              = "{\"value\":\"newvalue\"}"
+  dedicated_acct_host_only   = true
+  local_disk                 = false
+
+  provisioner "remote-exec" {
+    inline = [
+      "echo 'Ejecutando script en la instancia ${self.private_ip}'",
+      "ssh user@${self.private_ip} 'sh /ruta/al/script.sh'"
+    ]
+  }
+}
+
+resource "ibm_compute_vm_instance" "virtual-server-classic-host" {
+  count                      = local.server_count_host
+  hostname                   = "host-virtual-server-${count.index + 1}"
+  domain                     = "IBM-PoC-Landing-Zone-Enterprise.cloud"
+  os_reference_code          = "REDHAT_8_64"
+  datacenter                 = "dal13"
+  network_speed              = 10
+  hourly_billing             = false
+  private_network_only       = false
+  cores                      = 4
+  memory                     = 16384
+  disks                      = [25, 250]
+  user_metadata              = "{\"value\":\"newvalue\"}"
+  dedicated_acct_host_only   = true
+  local_disk                 = false
+
+  provisioner "remote-exec" {
+    inline = [
+      "echo 'Ejecutando script en la instancia ${self.private_ip}'",
+      "ssh user@${self.private_ip} 'sh /ruta/al/script.sh'"
+    ]
+  }
+  
+}
